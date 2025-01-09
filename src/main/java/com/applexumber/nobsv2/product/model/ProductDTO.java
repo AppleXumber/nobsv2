0c1d15ad9bfc1974b5ed1451b00deadaf43fd660
AppleXumber <wayron.jhonny@gmail.com>
2025-01-08 21:42:54 -0300

    Part 7: Unit testing

--- src/main/java/com/applexumber/nobsv2/product/model/ProductDTO.java
+++ src/main/java/com/applexumber/nobsv2/product/model/ProductDTO.java
@@ -1,9 +1,23 @@
 package com.applexumber.nobsv2.product.model;
 
+import java.util.Objects;
+
 public class ProductDTO {
     private Integer id;
     private String name;
     private String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductDTO that = (ProductDTO) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
 
     public Integer getId() {
         return id;
